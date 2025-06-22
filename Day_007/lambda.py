f8f07501337edb3d746ea9b41bbe099581f21917
Vilker Rainer <142421864+vilkerrainer@users.noreply.github.com>
2025-06-22 11:37:37 -0300

    Dia 7 concluido

---
+++ Day_007/lambda.py
@@ -0,0 +1,10 @@
+"""
+Funções Lambda + sorted:
+TODO: Ordene uma lista de tuplas (nome, idade) pelo segundo elemento (idade) usando função lambda.
+"""
+
+lista_de_pessoas = [('Carlos', 30), ('Ana', 25), ('Bruno', 35), ('Zoe', 22)]
+
+lista_ordenada_por_idade = sorted(lista_de_pessoas, key=lambda item: item[1])
+
+print(lista_ordenada_por_idade)
