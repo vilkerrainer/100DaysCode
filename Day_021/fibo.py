dedcbbb1de2839748674b663837af8d0735da407
Vilker Rainer <142421864+vilkerrainer@users.noreply.github.com>
2025-07-06 02:43:25 -0300

    Dia 21 concluido

---
+++ Day_021/fibo.py
@@ -0,0 +1,20 @@
+"""
+Geradores (generators):
+TODO: Crie um gerador que gere os números da sequência de Fibonacci até um limite.
+"""
+
+def gerador_fibonacci(limite):
+    a, b = 0, 1
+    while a <= limite:
+        yield a
+        a, b = b, a + b
+
+limite_maximo = 100
+
+print(f"Gerando a sequência de Fibonacci até {limite_maximo}:")
+
+fib_gen = gerador_fibonacci(limite_maximo)
+
+for numero in fib_gen:
+    print(numero, end=" ")
+    
