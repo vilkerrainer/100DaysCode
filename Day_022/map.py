3c76451972fdaa7ca08bc561316ce7227ca15fcc
Vilker Rainer <142421864+vilkerrainer@users.noreply.github.com>
2025-07-07 12:52:54 -0300

    Dia 22 concluido

---
+++ Day_022/map.py
@@ -0,0 +1,8 @@
+"""
+Funções Anônimas e Map:
+TODO: Use map() para converter uma lista de strings para maiúsculas.
+"""
+
+lista = ['python', 'dias', 'meses', 'verbo']
+resultado = list(map(lambda x: x.upper(), lista))
+print(resultado)
